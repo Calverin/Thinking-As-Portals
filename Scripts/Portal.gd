169649017e4a8f44ce400ffb117e9905902b97e8
ChipChase <138906435+ChipChase@users.noreply.github.com>
2023-07-08 15:06:25 -0400

    when box goes through portal it goes out the right direction

--- Scripts/Portal.gd
+++ Scripts/Portal.gd
@@ -4,8 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-var nodePath
-
+signal Orange_direction(bool)
+var blue_direction: bool
 var box_posistion
 
 @export var active = false
@@ -32,15 +32,17 @@ func _physics_process(delta):
 			velocity.y = JUMP_VELOCITY
 	
 		# Get the input direction and handle the movement/deceleration.
-		var direction = Input.get_axis("left", "right")
-		if direction:
+		var Orange_direction = Input.get_axis("left", "right")
+		if Orange_direction:
 			# Set facing direction
-			if direction < 0:
+			if Orange_direction < 0:
 				sprite.flip_h = true
+				emit_signal("Orange_direction",true)
 			else:
 				sprite.flip_h = false
+				emit_signal("Orange_direction",false)
 				
-			velocity.x = move_toward(velocity.x, direction * SPEED, SPEED / 10)
+			velocity.x = move_toward(velocity.x, Orange_direction * SPEED, SPEED / 10)
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED / 5)
 	
@@ -57,12 +59,28 @@ func _physics_process(delta):
 		var collision := get_slide_collision(index)
 		if (collision.get_collider().name == "Box"):
 			other_portal_postion = get_node("/root/ROOT/World/Blue Portal").position
-			box_posistion = get_node("/root/ROOT/World/Box").position
+			box_posistion = other_portal_postion
+			
 			print("the box position is", box_posistion)
 			print("the blue portal position is", other_portal_postion)
 			print("I collided with ", collision.get_collider().name)
-			get_node("/root/ROOT/World/Box").position = other_portal_postion
+			get_node("/root/ROOT/World/Box").position = box_posistion
+			if (blue_direction == true):
+				
+				get_node("/root/ROOT/World/Box").position.x -= 40
+			elif(blue_direction == false):
+				get_node("/root/ROOT/World/Box").position.x += 40
+		
+			
 			
 func teleport_box(other_portal_postion: Node2D) -> void:
 	var box_posistion = get_node("Box")
 	box_posistion.position = other_portal_postion.position 
+
+
+func _on_blue_portal_blue_direction(bool):
+	if(bool == true):
+		blue_direction = true
+	elif(bool == false):
+		blue_direction = false
+		
